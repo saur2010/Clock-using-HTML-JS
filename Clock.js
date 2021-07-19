e52404035bd248f0bc5bfa2d24661165de2af1d5
Saurish <saurishwebwork@gmail.com>
2021-07-19 12:43:43 +0530

    Clock html/js

---
+++ Clock.js
@@ -0,0 +1,9 @@
+var a;
+var date;
+var time;
+setInterval(() => {
+    a = new Date();
+    date = a.toLocaleDateString();
+    time = a.getHours() + ':' + a.getMinutes() + ':' + a.getSeconds();
+    document.getElementById('time').innerHTML = time + ' on ' + date;
+}, 1000);
